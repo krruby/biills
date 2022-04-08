1a01800d3f4f6767674b573b5bd7fd452507f531
krruby <103266375+krruby@users.noreply.github.com>
2022-04-08 19:58:24 +0530

    Add files via upload

---
+++ bill/src/main/java/com/bills/bill/BillsController.java
@@ -0,0 +1,42 @@
+package com.bills.bill;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class BillsController {
+
+    @GetMapping("/bills/{id}")
+    ResponseEntity<String> getBill(@PathVariable int id){
+        return ResponseEntity.ok("Total number of coins : "+ (int)coinChange(id));
+    }
+
+    public float coinChange( int amount) {
+        float min= howSumBest(amount,coins);
+        return (min==Integer.MAX_VALUE-1)?-1:min;
+    }
+    int[] bills = new int[]{1,2,5,10,20,50,100};
+    float[] coins = new float[]{0.01f ,0.05f, 0.10f, 0.25f};
+    Map<Float, Integer> map= new HashMap<Float, Integer>();
+
+    int howSumBest(float k, float arr[]) {
+        if (map.containsKey(k))
+            return map.get(k);
+        if (k == 0) return 0;
+        if (k < 0) return Integer.MAX_VALUE-1;
+        int c=Integer.MAX_VALUE-1;
+        for (int i = 0; i < arr.length; i++) {
+            int min=howSumBest(k - arr[i], arr);
+            if(min==Integer.MAX_VALUE)
+                c= Math.min(c,min);
+            c= Math.min(c,min+1);
+        }
+        map.put(k, c);
+        return c;
+    }
+}
