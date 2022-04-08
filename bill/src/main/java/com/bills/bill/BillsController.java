f4e10c1a628371b44e9305598c9a19bf225dcd02
krruby <103266375+krruby@users.noreply.github.com>
2022-04-08 20:26:51 +0530

    Update BillsController.java

--- bill/src/main/java/com/bills/bill/BillsController.java
+++ bill/src/main/java/com/bills/bill/BillsController.java
@@ -18,10 +18,12 @@ public class BillsController {
 
     public float coinChange( int amount) {
         float min= howSumBest(amount,coins);
+        cnt = new int[]{100,100,100,100,};
         return (min==Integer.MAX_VALUE-1)?-1:min;
     }
     int[] bills = new int[]{1,2,5,10,20,50,100};
     float[] coins = new float[]{0.01f ,0.05f, 0.10f, 0.25f};
+    int[] cnt = new int[]{100,100,100,100};
     Map<Float, Integer> map= new HashMap<Float, Integer>();
 
     int howSumBest(float k, float arr[]) {
@@ -31,9 +33,11 @@ public class BillsController {
         if (k < 0) return Integer.MAX_VALUE-1;
         int c=Integer.MAX_VALUE-1;
         for (int i = 0; i < arr.length; i++) {
+            if(cnt[i]==0) continue;
+            cnt[i]--;
             int min=howSumBest(k - arr[i], arr);
-            if(min==Integer.MAX_VALUE)
-                c= Math.min(c,min);
+            cnt[i]++;
+            if(min==Integer.MAX_VALUE) c= Math.min(c,min);
             c= Math.min(c,min+1);
         }
         map.put(k, c);
